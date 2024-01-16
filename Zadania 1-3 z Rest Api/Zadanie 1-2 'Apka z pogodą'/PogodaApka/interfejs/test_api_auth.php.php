40b09df8ae074b8584f7814ac995f8e2b9f8aa2f
Foke4y <156709163+Foke4y@users.noreply.github.com>
2024-01-16 17:06:06 +0100

    Add files via upload

---
+++ Zadania 1-3 z Rest Api/Zadanie 1-2 'Apka z pogodą'/PogodaApka/interfejs/test_api_auth.php.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$weather = "";
+$error = "";
+if(isset($_POST['city'])){
+    $city = urlencode($_POST['city']);
+    $urlContent = file_get_contents('https://api.openweathermap.org/data/2.5/weather?q=' . $_GET['city'] . '&units=metric&appid=dcd19bf3ff4d7ad39cd884de6d0f3f00');
+    $forecastArray = json_decode($urlContent, true);
+
+    if($forecastArray['cod'] == 200){
+        $weather = 'Pogoda w ' . $_POST['city'] . ': ' . $forecastArray['weather'][0]['description'];
+        $weather = $weather. '<br>Predkosc powietrza:  ' .  $forecastArray['wind']['speed'] . 'm/s';
+        $weather = $weather. '<br>Temperatura:  ' .  $forecastArray['main']['temp'] . '&#8451';
+    } else {
+        $error = "Nie prawidlowa nazwa Miasta";
+    }
+}
+
+?>
+<!doctype html>
+<html lang="PL">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../styles/style.css"
+
+</head>
+<body>
+<title>PogodaApka</title>
+
+<div class="container" id="MainDiv">
+    <h1>Pogoda w Miastach</h1>
+    <form>
+        <div class="form-group">
+            <label for="city">Wybierz swoje Miasto</label>
+            <input class="form-control" id="city" name="city" aria-describedby="Wybierz" placeholder="Miasto">
+        </div>
+        <button type="submit" class="btn btn-primary">Sprawdz</button>
+    </form>
+    <div>
+        <?php
+        if($weather){
+            echo '<div class="alert alert-secondary" role="alert">'. $weather  . '</div>';
+        } else if ($error) {
+            echo '<div class="alert alert-danger" role="alert">'. $error . '</div>';
+        }
+        ?>
+
+    </div>
+
+</body>
+</html>
---
+++ Zadania 1-3 z Rest Api/Zadanie nr3/WysylaniePost.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+
+    if (preg_match('/\d/', $name) || preg_match('/\d/', $surname)) {
+        echo 'Imię lub nazwisko zawiera cyfry :).';
+    } else {
+        $postData = array(
+            'Imię' => $name,
+            'Nazwisko' => $surname,
+            'Wiek' => $age,
+            'Plec' => $gender,
+        );
+        $postDataString = http_build_query($postData);
+
+        $contextOptions = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => $postDataString
+            )
+        );
+
+        $context = stream_context_create($contextOptions);
+
+        $url = 'https://jsonplaceholder.typicode.com/posts'; // Исправлен URL
+        $response = file_get_contents($url, false, $context);
+
+        echo $response;
+    }
+}
+?>
